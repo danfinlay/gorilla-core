7549f5fc30f2107477401abb30f5a3ddca0d0306
Dan Finlay <dan.finlay@consensys.net>
2023-09-20 12:19:48 -0700

    Cleanup main page

--- src/main.ts
+++ src/main.ts
@@ -17,19 +17,11 @@ import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Gorilla Core</h1>
+    <p>An extensible JavaScript kernel for building secure applications</p>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
 
